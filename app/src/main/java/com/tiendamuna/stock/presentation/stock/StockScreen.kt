91e5847827870019f3a8d592b92933591d8e4e90
criswerner <cristianwer@gmail.com>
2026-08-11 15:57:30 -0300

    Move hardcoded texts to resources

--- app/src/main/java/com/tiendamuna/stock/presentation/stock/StockScreen.kt
+++ app/src/main/java/com/tiendamuna/stock/presentation/stock/StockScreen.kt
@@ -2,7 +2,6 @@ package com.tiendamuna.stock.presentation.stock
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
